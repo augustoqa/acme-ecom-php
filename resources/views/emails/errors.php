ddb2def86a15568b00ec219dd90157b0fc50cb3e
Cesar Acual <chechaacual@gmail.com>
2020-08-25 22:28:25 -0600

    Error handling

---
+++ resources/views/emails/errors.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <div style="color: white; width: 600px; padding: 15px; margin: 0 auto; background-color: #0a0a0a; color: #fff">
+        <img src="https://via.placeholder.com/150">
+
+        <?php echo "Error: {$data}" ?>
+
+        <p>
+            Regards <br><br>
+            <strong>Devscreencast Team</strong>
+        </p>
+    </div>
+</body>
+</html>
